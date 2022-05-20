8f093dee321ae1fa6810617fbb4bd8cdfea058eb
yuya-kambayashi <yuya-kambayashi@kke.co.jp>
2022-05-20 16:05:45 +0900

    JaxRsの追加

---
+++ src/main/java/main/java/fish/payara/jakarta/ee9/start/jpa/JaxRsActivator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.fish.payara.jakarta.ee9.start.jpa;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+/**
+ *
+ * @author yuya-kambayashi
+ */
+@ApplicationPath("/rest")
+public class JaxRsActivator {
+    
+}
