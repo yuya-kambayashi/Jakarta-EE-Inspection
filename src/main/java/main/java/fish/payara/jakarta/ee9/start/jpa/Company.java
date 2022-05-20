e1e968d16966581dd27ee09eb2c0817277a3019c
yuya-kambayashi <yuya-kambayashi@kke.co.jp>
2022-05-20 15:38:09 +0900

    entityクラスを追加します

---
+++ src/main/java/main/java/fish/payara/jakarta/ee9/start/jpa/Company.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.fish.payara.jakarta.ee9.start.jpa;
+
+import java.io.Serializable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+/**
+ *
+ * @author yuya-kambayashi
+ */
+@Entity
+@Table(name = "company")
+public class Company implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Column(name = "id")
+    private Long id;
+    
+    @Column(name = "name")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Company)) {
+            return false;
+        }
+        Company other = (Company) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "main.java.Company[ id=" + id + " ]";
+    }
+    
+}
---
+++ src/main/java/main/java/fish/payara/jakarta/ee9/start/jpa/CompanyService.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.fish.payara.jakarta.ee9.start.jpa;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+/**
+ *
+ * @author yuya-kambayashi
+ */
+@ApplicationScoped
+public class CompanyService {
+    
+    public Company findCompany(Long id){
+        
+    }
+    
+}
