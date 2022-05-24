7b15a5a4b738fed35feb0a1221d6ad8040ebdd01
yuya-kambayashi <yuya-kambayashi@kke.co.jp>
2022-05-24 09:15:34 +0900

    testに対するbeanを追加します

---
+++ src/main/java/yuyakambayashi/ui/testBean.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package yuyakambayashi.ui;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.Dependent;
+
+/**
+ *
+ * @author yuya-kambayashi
+ */
+@Named(value = "testBean")
+@Dependent
+public class testBean {
+
+    /**
+     * Creates a new instance of testBean
+     */
+    public testBean() {
+    }
+    
+}
